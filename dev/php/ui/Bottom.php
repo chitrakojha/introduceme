8355f94ec3835457a5a1b5d3f28125cbd3c4d152
Keegan Street <keeganstreet@gmail.com>
2011-08-28 16:28:35 +0200

    removed uservoice widget. nobody was using it. it just added to page bloat. there is a hyperlink in the footer anyway and that will suffice

--- dev/php/ui/Bottom.php
+++ dev/php/ui/Bottom.php
@@ -27,13 +27,6 @@ class Bottom {
 				// Google Analytics
 				'var _gaq = [["_setAccount", "UA-20937143-1"],["_trackPageview"]];'.
 				'(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;g.src="//www.google-analytics.com/ga.js";s.parentNode.insertBefore(g,s);}(document,"script"));'.
-				// Uservoice
-				'var uservoiceOptions = { key: "introduceme", host: "introduceme.uservoice.com", forum: "99481", alignment: "left", background_color:"#152061", text_color: "white", hover_color: "#0f1644", lang: "'.Content::l().'", showTab: true };'.
-				'if (!introduceme.mobile) {'.
-					'$(document).ready(function () {'.
-						'(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;g.src="//cdn.uservoice.com/javascripts/widgets/tab.js";s.parentNode.insertBefore(g,s);}(document,"script"));'.
-					'});'.
-				'}'.
 			'</script>'.
 
 			'</body>'.
